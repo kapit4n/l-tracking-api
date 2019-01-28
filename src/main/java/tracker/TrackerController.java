cb66aabe44fa3a6717c33e0f0f66cf8ff53f147e
Luis Arce <luis.arce22@gmail.com>
2019-01-28 11:41:00 -0400

    Adding CrosssOrigin to tracker controller

--- src/main/java/tracker/TrackerController.java
+++ src/main/java/tracker/TrackerController.java
@@ -1,9 +1,12 @@
 package tracker;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8081", maxAge = 3600)
 @RestController
 public class TrackerController {
 
